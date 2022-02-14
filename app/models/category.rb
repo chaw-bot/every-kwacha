97a69ccd192da324a6e89c83498ef3282563ccb1
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-14 03:29:09 +0200

    Generate models

---
+++ app/models/category.rb
@@ -0,0 +1,12 @@
+class Category < ApplicationRecord
+  belongs_to :user
+  has_many :category_records
+  has_many :records, through: :category_records
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :icon, presence: true, length: { maximum: 250 }
+
+  def recent_records
+    records.order(created_at: :DESC)
+  end
+end
---
+++ app/models/record.rb
@@ -0,0 +1,8 @@
+class Record < ApplicationRecord
+  belongs_to :user
+  has_many :category_records
+  has_many :categories, through: :category_records
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+end
