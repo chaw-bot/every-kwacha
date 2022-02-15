ffcf63d9af6154dbc3022a6c19925aee84c59d8b
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-15 16:59:57 +0200

    Add index actiong to Categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   load_and_authorize_resource
-  def index; end
+
+  def index
+    @categories = current_user.categories
+  end
 
   def new; end
 
