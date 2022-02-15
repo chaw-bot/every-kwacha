ed7eec93399e4e3de1900e0c0bd9eb62374a851d
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-15 16:59:13 +0200

    Redirect to home page after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(User)
+        categories_path
+      else
+        super
+      end
+  end
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
