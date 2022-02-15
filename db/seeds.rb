abc5efb95a751f0c9597cbbda260011254fa5524
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-15 16:58:35 +0200

    Add some data to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create!(name: 'John Doe', email: 'john@mail.com', password: 'john123', confirmed_at: Time.now)
+
+icon1 = 'https://png.pngitem.com/pimgs/s/18-186729_food-beverage-food-and-beverage-icon-hd-png.png'
+icon2 = 'https://www.pngitem.com/pimgs/m/264-2647646_transparent-fashion-icon-png-fashion-designing-icon-png.png'
+icon3 = 'http://cdn.onlinewebfonts.com/svg/img_445017.png'
+icon4 = 'https://cdn-icons-png.flaticon.com/512/2270/2270989.png'
+icon5 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScyOyz56At0KjzlZvQXcp6VtPGnBwYbmCe5Q&usqp=CAU'
+
+
+category1 = user.categories.create!(name: 'Food', icon: icon1)
+category2 = user.categories.create!(name: 'Fashion', icon: icon2)
+category3 = user.categories.create!(name: 'Health', icon: icon3)
+category4 = user.categories.create!(name: 'Sport', icon: icon4)
+category5 = user.categories.create!(name: 'Entertainment', icon: icon5)
