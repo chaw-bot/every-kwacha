6d86573cb052f4e2d8adc1e79c44d79b3c08ecd4
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-16 15:14:26 +0200

    Edit index method

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,8 +1,15 @@
 class RecordsController < ApplicationController
   load_and_authorize_resource
-  def index; end
+
+  def index
+    @category = current_category
+  end
 
   def new; end
 
   def create; end
+
+  def current_category
+    @categories = current_user.categories.includes(:records).find(params[:category_id])
+  end
 end
