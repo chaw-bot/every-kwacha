ee203114e5fa1938e0ed4843fe52258b8b25c89e
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-16 17:29:33 +0200

    Display total amount

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 user = User.create!(name: 'John Doe', email: 'john@mail.com', password: 'john123', confirmed_at: Time.now)
 
-icon1 = 'https://png.pngitem.com/pimgs/s/18-186729_food-beverage-food-and-beverage-icon-hd-png.png'
-icon2 = 'https://www.pngitem.com/pimgs/m/264-2647646_transparent-fashion-icon-png-fashion-designing-icon-png.png'
-icon3 = 'http://cdn.onlinewebfonts.com/svg/img_445017.png'
-icon4 = 'https://cdn-icons-png.flaticon.com/512/2270/2270989.png'
-icon5 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScyOyz56At0KjzlZvQXcp6VtPGnBwYbmCe5Q&usqp=CAU'
+icon1 = 'https://freedesignfile.com/upload/2020/09/Tasty-food-vector-icon.jpg'
+icon2 = 'https://thumbs.dreamstime.com/b/fashion-boutique-rgb-color-icon-women-stylish-garment-online-store-shopping-mall-category-female-clothes-shoes-accessories-187269539.jpg'
+icon3 = 'https://www.seekpng.com/png/detail/134-1349786_click-the-icons-below-for-more-info-on.png'
+icon4 = 'https://image.emojipng.com/679/10488679.jpg'
+icon5 = 'https://cdn-icons-png.flaticon.com/512/1974/1974756.png'
 
 
 category1 = user.categories.create!(name: 'Food', icon: icon1)
 category2 = user.categories.create!(name: 'Fashion', icon: icon2)
 category3 = user.categories.create!(name: 'Health', icon: icon3)
 category4 = user.categories.create!(name: 'Sport', icon: icon4)
-category5 = user.categories.create!(name: 'Accessories', icon: icon5)
+category5 = user.categories.create!(name: 'Music', icon: icon5)
 
 transaction1 = category4.records.create!(user: user, name: 'Trainers', amount: 152 )
-transaction2 = category5.records.create!(user: user, name: 'Watch', amount: 500 )
-transaction3 = category5.records.create!(user: user, name: 'Necklace', amount: 45 )
+transaction2 = category5.records.create!(user: user, name: 'Vinyl', amount: 500 )
+transaction3 = category5.records.create!(user: user, name: 'Concert tickets', amount: 45 )
 transaction4 = category4.records.create!(user: user, name: 'Football', amount: 56 )
 transaction5 = category4.records.create!(user: user, name: 'Back pack', amount: 100 )
