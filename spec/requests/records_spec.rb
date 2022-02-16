0c20322492ba5e4fe33d7cfda0991ce6ad6b290d
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-16 23:06:03 +0200

    Test controllers

--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Records', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should have a response status code of 200 for success' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the correct template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
   end
 end
